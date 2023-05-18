116c54240cfed0a7487f478a84b684fac19d39ab
dronarnia-admin <115626469+dronarnia-admin@users.noreply.github.com>
2023-05-18 14:27:05 +0300

    Update feature-post.js
    Added space between buttons

--- src/templates/feature-post.js
+++ src/templates/feature-post.js
@@ -43,7 +43,8 @@ export const FeaturePostTemplate = ({
             <section className="section">
               <div className="has-text-centered">
                 <Link className="button is-warning is-large is-responsive" to="/donate">Підтримати</Link>
-                <a className="button is-info is-large is-responsive is-light" href="https://airtable.com/shrjr9gavatDWRilm" target="_blank" rel="noopener noreferrer" >Подати заявку</a>
+                <span style={{ margin: '0 20px' }}></span>
+                <a className="button is-large is-responsive is-info" href="https://airtable.com/shrjr9gavatDWRilm" target="_blank" rel="noopener noreferrer" >Подати заявку</a>
               </div>
             </section>
 
